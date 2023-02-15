da6371a7c8495184727dbdfcc0a52698119aa047
annenyc <annelau.nyc@gmail.com>
2023-02-15 06:22:55 +0000

    anne-photogram-first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
+get("/users", { :controller => "users", :action => "index"})
+
+get("/users/:username", {:controller => "users", :action: => "user_details"})
+
+
 
 end
