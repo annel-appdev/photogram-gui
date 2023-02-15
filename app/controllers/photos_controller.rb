e67396e2ce7bcf5a6bd09f0c36afe31f1bad6125
annenyc <annelau.nyc@gmail.com>
2023-02-15 10:36:15 +0000

    anne-photogram-photo create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,39 @@ class PhotosController < ApplicationController
     render({ :template => "photo_templates/index.html.erb"})
   end
 
+  def photo_details
+    #Parameters: {"photo_id"=>"842"}
+    url_id = params.fetch("photo_id")
+    matching_photo = Photo.where({ :id => url_id})
+    @the_photo = matching_photo.at(0)
+  render({ :template => "photo_templates/photo_details.html.erb"})
+  end
+
+  def delete_photo
+    #Parameters: {"photo_id"=>"919;"}
+    the_id = params.fetch("photo_id")
+    matching_photos = Photo.where({ :id => the_id})
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+  #render({ :template => "photo_templates/delete_photo.html.erb"})
+    redirect_to("/photos")
+  end
+
+  def add_photo
+    #Parameters: {"query_image"=>"https://media.thetot.com/wp-content/uploads/2019/03/18133538/LITTLELIGHTSROBOTLAMPNAVYBLUERED.jpg", "query_caption"=>"Hello world!", "query_owner_id"=>"117"}
+
+  input_image = params.fetch("query_image")
+  input_caption = params.fetch("query_caption")
+  input_owner_id = params.fetch("query_owner_id")
+
+  new_photo = Photo.new
+  new_photo.image = input_image
+  new_photo.caption = input_caption
+  new_photo.owner_id = input_owner_id
+
+  new_photo.save
+
+    #render({ :template => "photo_templates/add_photo.html.erb"})
+    redirect_to("/photos/" + new_photo.id.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ get("/users/:username", { :controller => "users", :action => "user_details" })
 
 get("/photos", { :controller => "photos", :action => "index" })
 
+get("/photos/:photo_id", { :controller => "photos", :action => "photo_details" })
+
+get("/delete_photo/:photo_id", { :controller => "photos", :action => "delete_photo"})
+
+get("/insert_photo", { :controller => "photos", :action => "add_photo"})
 end
